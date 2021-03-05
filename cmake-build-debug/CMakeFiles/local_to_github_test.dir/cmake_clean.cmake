96043a0ab4e2f601e65156510aa9f41008e4ce1d
rawazer <roireit@gmail.com>
2021-03-05 17:13:46 +0200

    initializing repository

---
+++ cmake-build-debug/CMakeFiles/local_to_github_test.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/local_to_github_test.dir/main.cpp.o"
+  "local_to_github_test"
+  "local_to_github_test.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/local_to_github_test.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
